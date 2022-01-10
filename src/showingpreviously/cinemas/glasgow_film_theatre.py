8cbfbb9018fdb0d533b6bea68a2a31c236bebb5b
Andrew Ferguson <andrewferguson500@gmail.com>
2022-01-10 19:03:03 +0000

    Glasgow Film Theatre: code cleanup

--- src/showingpreviously/cinemas/glasgow_film_theatre.py
+++ src/showingpreviously/cinemas/glasgow_film_theatre.py
@@ -1,8 +1,10 @@
-from showingpreviously.model import Chain, Cinema, Screen, Film, Showing, ChainArchiver, CinemaArchiverException
-from showingpreviously.consts import UNKNOWN_FILM_YEAR, STANDARD_DAYS_AHEAD, UK_TIMEZONE
-import showingpreviously.requests as requests
+import bs4
+import datetime
+import re
 
-import datetime, json, re, pytz, bs4
+import showingpreviously.requests as requests
+from showingpreviously.consts import UNKNOWN_FILM_YEAR, UK_TIMEZONE
+from showingpreviously.model import Chain, Cinema, Screen, Film, Showing, ChainArchiver, CinemaArchiverException
 
 CHAIN = Chain(name='Glasgow Film Theatre')
 CINEMA = Cinema(name='Glasgow Film Theatre', timezone=UK_TIMEZONE)
@@ -22,14 +24,7 @@ class GlasgowFilmTheatre(ChainArchiver):
         showings = []
         req = get_response(WHATS_ON_URL)
         soup = bs4.BeautifulSoup(req.text, 'html.parser')
-        current_year = datetime.datetime.now().year
         for showings_on_date in soup.find_all('div', class_='show-strip-group'):
-            #showing_date_str = showings_on_date.find('div, class_='show-strip-group-date').text.strip()
-            #if showing_date_str.lower() == 'today':
-            #    # dealing with 'today' is complicated, because the archiver could be running
-            #    # in a timezone different to that of the cinema, and so the date of 'today'
-            #    # for the cinema may be different to the 'today' of the archiver
-            #    showing_date_str = datetime.datetime.now(pytz.timezone(CINEMA.timezone)).strftime('%a %-d %B')
             for showing_film in showings_on_date.find_all('div', class_='show-strip'):
                 film_heading = showing_film.find('div', class_='show-strip-heading')
                 film, film_attributes = film_from_heading(film_heading)
@@ -43,12 +38,12 @@ class GlasgowFilmTheatre(ChainArchiver):
                     showing_screen = Screen(showing_item['data-instance-venue'])
                     showing_attributes = get_showing_attributes(showing_item)
                     showing = Showing(
-                        film = film,
-                        time = showing_datetime,
-                        chain = CHAIN,
-                        cinema = CINEMA,
-                        screen = showing_screen,
-                        json_attributes = {**film_attributes, **showing_attributes}
+                        film=film,
+                        time=showing_datetime,
+                        chain=CHAIN,
+                        cinema=CINEMA,
+                        screen=showing_screen,
+                        json_attributes={**film_attributes, **showing_attributes}
                     )
                     showings.append(showing)
         return showings
@@ -126,7 +121,7 @@ def film_from_heading(film_heading: bs4.element.Tag) -> (Film, dict[str, any]):
                 film_attributes['subtitled'] = with_subtitles.group(2).strip()
             else:
                 film_attributes['language'] = meta_value
-    if film == None:
+    if film is None:
         film = Film(name=film_title, year=UNKNOWN_FILM_YEAR)
     films_cache[film_title] = film, film_attributes
     return film, film_attributes
@@ -143,6 +138,5 @@ def get_showing_attributes(showing_event: bs4.element.Tag) -> dict[str, any]:
         elif attr_name == 'Dementia friendly':
             showing_attributes['dementia-friendly'] = True
         else:
-            raise Exception(f'Unknown attribute {attr_name}')
+            raise CinemaArchiverException(f'Unknown attribute {attr_name}')
     return showing_attributes
-
