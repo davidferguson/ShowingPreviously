d2ca258940e8bf6eaeb6417875d32e76328b2253
andrewferguson <andrewferguson500@gmail.com>
2022-11-30 18:11:32 +0000

    CLI: Add "--dummy-run" option to not make any changes to DB (#36)
    * CLI: Add "--dummy-run" option to not make any changes to DB
    
    The `--dummy-run` option runs the archiver as normal, but does not
    save any of the results into the DB. This is useful in a number of
    circumstances:
    
    1. Development: during development it is essential to know that
       the code compiles and runs without errors, but before this is
       known, having partial or incomplete results saved in the DB
       is undesirable.
    
    2. Testing: automated checks to ensure that there have been no
       breaking API changes are desirable, but such tests should not
       change a DB.
    
    Remaining issues: the URL log that maintains a list of URLs that
    have been accessed to perform the archiving is not affected by the
    status of the `--dummy-run` flag. This is undesirable, however I
    am not sure what the most elegent solution to fixing this would be.
    
    * Rename "dummy_run" flag to "dry_run"
    
    * Set `dry_run` flag to be default false in the source code

--- src/showingpreviously/archiver.py
+++ src/showingpreviously/archiver.py
@@ -31,7 +31,7 @@ all_cinema_chains = [
 ]
 
 
-def process_showing(showing: Showing):
+def process_showing(showing: Showing, dry_run: bool = False):
     film = showing.film
     time = showing.time
     chain = showing.chain
@@ -42,27 +42,28 @@ def process_showing(showing: Showing):
     timezone = pytz.timezone(cinema.timezone)
     utc_time = timezone.localize(time).astimezone(pytz.timezone('UTC'))
 
-    add_chain(chain.name)
-    add_cinema(chain.name, cinema.name, cinema.timezone)
-    add_screen(chain.name, cinema.name, screen.name)
-    add_film(film.name, film.year)
-    add_showing(film.name, film.year, chain.name, cinema.name, screen.name, utc_time, json_attributes)
+    if not dry_run:
+        add_chain(chain.name)
+        add_cinema(chain.name, cinema.name, cinema.timezone)
+        add_screen(chain.name, cinema.name, screen.name)
+        add_film(film.name, film.year)
+        add_showing(film.name, film.year, chain.name, cinema.name, screen.name, utc_time, json_attributes)
 
 
-def run_chain(chain: ChainArchiver):
+def run_chain(chain: ChainArchiver, dry_run: bool = False):
     showings = chain.get_showings()
     for showing in showings:
-        process_showing(showing)
+        process_showing(showing, dry_run)
 
 
-def run_all() -> None:
+def run_all(dry_run: bool = False) -> None:
     for cinema_chain in all_cinema_chains:
-        run_chain(cinema_chain)
+        run_chain(cinema_chain, dry_run)
     close_selenium_webdriver()
 
 
-def run_single(name: str) -> None:
+def run_single(name: str, dry_run: bool = False) -> None:
     for cinema_chain in all_cinema_chains:
         if type(cinema_chain).__name__ == name:
-            run_chain(cinema_chain)
+            run_chain(cinema_chain, dry_run)
     close_selenium_webdriver()
--- src/showingpreviously/cli.py
+++ src/showingpreviously/cli.py
@@ -27,15 +27,18 @@ def info_cmd(list_chains: bool) -> None:
 
 @cli.command('run')
 @click.option('--chain', default=None, show_default=True, type=click.STRING, help='The archiver class name to run')
-def run_cmd(chain: Optional[str]) -> None:
+@click.option('--dry-run', 'dry_run', is_flag=True, default=False, show_default=False, type=click.BOOL, help='Run, but don\'t make any changes to the DB')
+def run_cmd(chain: Optional[str], dry_run: bool = False) -> None:
     """Runs the archiver on all cinema chains"""
+    if dry_run:
+        print('Dry run, no changes to DB')
     if chain is None:
-        run_all()
+        run_all(dry_run)
     else:
         installed_chains = [type(cinema_chain).__name__ for cinema_chain in all_cinema_chains]
         if chain not in installed_chains:
             raise click.BadOptionUsage('--chain', f'No such installed chain "{chain}"')
-        run_single(chain)
+        run_single(chain, dry_run)
 
 
 if __name__ == '__main__':
